e51b4953fa118a2bd739101a060962d57067086d
Jameson <jamesxtw@gmail.com>
2022-07-22 12:56:00 +0800

    Bug fixed and code optimized

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,84 +4,110 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private float movementTimer=1;
-    private Vector2Int snakePosition;
-    // Start is called before the first frame update
-    private int snakeDirection = 0;
+    private float movementTimer;
+    private Vector2 snakePosition;
+    private Vector3 snakeDirection ;
+    private float snakeSpeed ;
+    private Vector2 snakeInputDirection ;
+    
     void Start()
     {
-        snakePosition = new Vector2Int(0,0);
-
+        movementTimer = 1;
+        snakePosition = new Vector2(0,0);
+        snakeSpeed = 0.5f;
+        snakeInputDirection = new Vector2(0, 0);
+        snakeDirection = new Vector3(0,snakeSpeed,0);
+        snakePosition = new Vector2(snakeDirection.x, snakeDirection.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (snakeDirection != 2)
-            {
-                snakeDirection = 0; //0=up
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (snakeDirection != 3)
-            {
-                snakeDirection = 1; //1=right
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (snakeDirection != 0)
-            {
-                snakeDirection = 2; //2=down
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (snakeDirection != 1)
-            {
-                snakeDirection = 3; //3=left
-            }
-        }
-      
+        Timer();
+        InputCheck();
+    }
 
+    void Timer()
+    {
         movementTimer -= Time.deltaTime;
-       
-        if(movementTimer <= 0)
+        if (movementTimer <= 0)
         {
             movementTimer = 1;
             timerEnded();
-            
         }
-        
     }
+
+    void InputCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            snakeInputDirection.x = 0;
+            snakeInputDirection.y = snakeSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            snakeInputDirection.x = snakeSpeed;
+            snakeInputDirection.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            snakeInputDirection.x = 0;
+            snakeInputDirection.y = -snakeSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            snakeInputDirection.x = -snakeSpeed;
+            snakeInputDirection.y = 0;
+        }
+    }
+
     void timerEnded()
     {
-        if(snakeDirection==0 && snakeDirection!= 2 )
+
+        if (snakeInputDirection.x == 0 && snakeInputDirection.y == snakeSpeed)
         {
-            snakePosition.y += 1;
-            
+            if (snakeDirection.y != -snakeSpeed)
+            {
+                snakeDirection.x = 0;
+                snakeDirection.y = snakeSpeed;
+                snakeDirection.z = 0;
+
+            }
         }
-       
-        if (snakeDirection == 1 && snakeDirection != 3)
+        if (snakeInputDirection.x == snakeSpeed && snakeInputDirection.y == 0)
         {
-            snakePosition.x += 1;
-            
+            if (snakeDirection.x != -snakeSpeed)
+            {
+                snakeDirection.x = snakeSpeed;
+                snakeDirection.y = 0;
+                snakeDirection.z = -90;
+            }
+
         }
-        if (snakeDirection == 2 && snakeDirection != 0)
+        if (snakeInputDirection.x == 0&& snakeInputDirection.y == -snakeSpeed)
         {
-            snakePosition.y -= 1;
-            
+            if (snakeDirection.y != snakeSpeed)
+            {
+                snakeDirection.x = 0;
+                snakeDirection.y = -snakeSpeed;
+                snakeDirection.z = 180;
+            }
         }
-        if (snakeDirection == 3 && snakeDirection != 1)
+        if (snakeInputDirection.x == -snakeSpeed && snakeInputDirection.y == 0)
         {
-            snakePosition.x -= 1;
-            
+            if (snakeDirection.x != snakeSpeed)
+            {
+                snakeDirection.x = -snakeSpeed;
+                snakeDirection.y = 0;
+                snakeDirection.z = 90;
+
+            }
         }
-        transform.position = new Vector2(snakePosition.x, snakePosition.y);
+
+        snakePosition.x = snakePosition.x + snakeDirection.x;
+        snakePosition.y = snakePosition.y + snakeDirection.y;
+        transform.position = new Vector2(snakePosition.x , snakePosition.y );
+        transform.eulerAngles=new Vector3( 0 , 0 , snakeDirection.z );
         return;
 
 
